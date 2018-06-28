7768072e07d9c264ed4a00007398572283df552f
markwh1te <iamwh1temark@outlook.com>
2018-06-28 15:14:06 +0800

    add top k quick partion solution

--- algorithms/acm/search.py
+++ algorithms/acm/search.py
@@ -1,4 +1,7 @@
-def topk(array, k):
+from random import choice
+
+
+def topk_min_stack(array, k):
     if len(array) == 1:
         return array[0]
     min_stack = sorted(array[:k])
@@ -9,3 +12,24 @@ def topk(array, k):
             min_stack.remove(max(min_stack))
             max_value = max(min_stack)
     return min_stack
+
+
+def topk_quick(array, k):
+    if len(array) <= 1:
+        return array
+    if k == 1:
+        return [min(array)]
+    pivot = choice(array)
+    left = []
+    right = []
+    for i in array:
+        if i < pivot:
+            left.append(i)
+        else:
+            right.append(i)
+    if len(left) == k:
+        return left
+    if len(left) < k:
+        return left + topk_quick(right, k-len(left))
+    if len(left) > k:
+        return topk_quick(left, k)
--- test/test_search.py
+++ test/test_search.py
@@ -6,4 +6,5 @@ def test_topk():
     array = [randint(1, 1000) for i in range(11, 200)]
     k = randint(1, 10)
     print(sorted(array)[:k], k, array)
-    assert sorted(search.topk(array, k)) == sorted(array)[:k]
+    assert sorted(array)[:k] == sorted(search.topk_min_stack(array, k))
+    assert sorted(array)[:k] == sorted(search.topk_quick(array, k))
