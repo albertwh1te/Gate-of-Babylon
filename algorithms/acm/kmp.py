ba41963e38a71955c713f6f5c16c36f177fa88bb
markwh1te <iamwh1temark@outlook.com>
2018-08-27 17:20:37 +0800

    add bruteforce match

--- algorithms/acm/kmp.py
+++ algorithms/acm/kmp.py
@@ -9,11 +9,10 @@ def bruteforce_match(s1: str, s2: str)->int:
     i = j = 0
     while i < len(s1) and j < len(s2):
         if s1[i] == s2[j]:
-            i = i + 1
             j = j + 1
         else:
-            i = i + 1
             j = 0
+        i = i + 1
 
     if j == len(s2):
         return i - j
