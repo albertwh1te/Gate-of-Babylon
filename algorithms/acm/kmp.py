b9402ac710f5fad0804082020cc23bc422901c70
markwh1te <iamwh1temark@outlook.com>
2018-08-27 16:27:29 +0800

    finished kmp algorithsms

--- algorithms/acm/kmp.py
+++ algorithms/acm/kmp.py
@@ -3,11 +3,36 @@ https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
 """
 
 
+def kmp(s1: str, s2: str)->int:
+    if s1 == None or s2 == None or len(s2) < 1 or len(s1) < len(s2):
+        return -1
+    next_arr = find_next(s2)
+    i = j = 0
+    while i < len(s1) and j < len(s2):
+        if s1[i] == s2[j]:
+            i = i + 1
+            j = j + 1
+        else:
+            if next_arr[j] == -1:
+                i = i + 1
+            else:
+                j = next_arr[j]
+
+    if j == len(s2):
+        # return the start index of substring
+        return i - j
+    else:
+        # sadly we donot find it :(
+        return -1
+
+
 def find_next(s: str)->[int]:
     """
     input:string
     output:the next array of string
     """
+    if len(s) == 1:
+        return [-1]
     result = [0 for i in range(len(s))]
     result[0] = -1
     result[1] = 0
@@ -22,7 +47,6 @@ def find_next(s: str)->[int]:
         else:
             result[i+1] = 0
         i = i + 1
-    print(result)
     return result
 
 
--- test/test_kmp.py
+++ test/test_kmp.py
@@ -1,7 +1,15 @@
-from algorithms.acm.kmp import find_next
+from algorithms.acm.kmp import find_next, kmp
 
 
 def test_find_next():
+    assert find_next("a") == [-1]
     assert find_next("ab") == [-1, 0]
     test_str1 = "abcabc"
     assert find_next(test_str1) == [-1, 0, 0, 0, 1, 2]
+
+
+def test_kmp():
+    assert kmp("", "") == -1
+    s1 = "abcabc"
+    s2 = "bc"
+    assert kmp(s1, s2) == s1.find(s2)
