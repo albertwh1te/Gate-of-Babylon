5e770184fc0222fc52204bffc424b5088f92d29c
markwh1te <iamwh1temark@outlook.com>
2018-08-27 16:57:30 +0800

    update help function name

--- algorithms/acm/kmp.py
+++ algorithms/acm/kmp.py
@@ -6,17 +6,17 @@ https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
 def kmp(s1: str, s2: str)->int:
     if s1 == None or s2 == None or len(s2) < 1 or len(s1) < len(s2):
         return -1
-    next_arr = find_next(s2)
+    pmt = find_pmt(s2)
     i = j = 0
     while i < len(s1) and j < len(s2):
         if s1[i] == s2[j]:
             i = i + 1
             j = j + 1
         else:
-            if next_arr[j] == -1:
+            if pmt[j] == -1:
                 i = i + 1
             else:
-                j = next_arr[j]
+                j = pmt[j]
 
     if j == len(s2):
         # return the start index of substring
@@ -26,10 +26,10 @@ def kmp(s1: str, s2: str)->int:
         return -1
 
 
-def find_next(s: str)->[int]:
+def find_pmt(s: str)->[int]:
     """
     input:string
-    output:the next array of string
+    output:the partial match table of string
     """
     if len(s) == 1:
         return [-1]
--- test/test_kmp.py
+++ test/test_kmp.py
@@ -1,11 +1,11 @@
-from algorithms.acm.kmp import find_next, kmp
+from algorithms.acm.kmp import find_pmt, kmp
 
 
-def test_find_next():
-    assert find_next("a") == [-1]
-    assert find_next("ab") == [-1, 0]
+def test_find_pmt():
+    assert find_pmt("a") == [-1]
+    assert find_pmt("ab") == [-1, 0]
     test_str1 = "abcabc"
-    assert find_next(test_str1) == [-1, 0, 0, 0, 1, 2]
+    assert find_pmt(test_str1) == [-1, 0, 0, 0, 1, 2]
 
 
 def test_kmp():
