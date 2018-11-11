029f7524178ff73340dfef6cb69df0378714b6f6
markwh1te <iamwh1temark@outlook.com>
2018-11-11 17:36:33 +0800

    add Dutch national flag problem

--- algorithms/acm/sort.py
+++ algorithms/acm/sort.py
@@ -1,8 +1,30 @@
 """Sorting Algorithms In Python
 """
 
+"""
+Dutch national flag problem
+https://en.wikipedia.org/wiki/Dutch_national_flag_problem
+"""
 
-def quick_sort(numbers):
+
+def sort_colors(nums: [int], pivot: int)->[int]:
+    small = 0
+    current = 1
+    big = len(nums)-1
+    while current < big:
+        if nums[current] == pivot:
+            current += 1
+        elif nums[current] > pivot:
+            nums[big], nums[current] = nums[current], nums[big]
+            big -= 1
+        else:
+            nums[current], nums[small] = nums[small], nums[current]
+            small += 1
+            current += 1
+    return nums
+
+
+def quick_sort(numbers: list):
     if len(numbers) <= 1:
         return numbers
     pivot = numbers[0]
--- test/test_sort.py
+++ test/test_sort.py
@@ -1,6 +1,7 @@
 from random import randint
 from algorithms.acm.sort import (
-    quick_sort
+    quick_sort,
+    sort_colors
 )
 
 
@@ -13,3 +14,31 @@ def test_quick_sort():
     length = randint(1, 103)
     array = [randint(1, 1311) for i in range(length)]
     assert(quick_sort(array)) == sorted(array)
+
+
+def test_sort_colors():
+    # test case 1
+    assert(sort_colors([], 0) == [])
+    # test case 2
+    array = [11, 2, 3, 4, 1, 7]
+    pivot = 2
+    result = sort_colors(array, pivot)
+    p_index = result.index(pivot)
+    for i, v in enumerate(result):
+        if v > pivot:
+            print(v, i)
+            assert(i > p_index)
+        elif v < pivot:
+            assert(i < p_index)
+
+    # test case 3
+    array = [11, 2, 3, 4, 4, 1, 7]
+    pivot = 4
+    result = sort_colors(array, pivot)
+    p_index = result.index(pivot)
+    for i, v in enumerate(result):
+        if v > pivot:
+            print(v, i)
+            assert(i > p_index)
+        elif v < pivot:
+            assert(i < p_index)
